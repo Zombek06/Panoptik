cbe92d55ca530fa33ef5da25e0a2dea04e20e4f2
Jozek <www@www.www>
2022-06-27 14:48:53 +0200

    dfdsdf

--- panop/settings.py
+++ panop/settings.py
@@ -122,5 +122,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+CSRF_TRUSTED_ORIGINS = ["https://zombek06.herokuapp.com/"]
+
+
 import django_heroku
 django_heroku.settings(locals())
